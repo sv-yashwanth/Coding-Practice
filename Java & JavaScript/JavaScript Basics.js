81c361cc25b034631ecaefe0e95af82e21ee2b93
SESHAGIRI VENKATA YASHWANTH <yashwanthhh34@gmail.com>
2026-06-01 12:34:43 +0530

    Add files via upload

---
+++ Java & JavaScript/JavaScript Basics.js
@@ -0,0 +1,35 @@
+let name = "Rahul";
+let age = 20;
+let marks = 85;
+
+console.log("Hello, World!");
+
+console.log("\nEntered Data");
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Marks:", marks);
+
+console.log("\nData Types");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof marks);
+
+let a = 10;
+let b = 5;
+
+console.log("\nArithmetic Operators");
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+
+console.log("\nRelational Operators");
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a == b:", a == b);
+
+console.log("\nLogical Operators");
+console.log("a > 0 && b > 0:", a > 0 && b > 0);
+console.log("a > 0 || b < 0:", a > 0 || b < 0);
+console.log("!(a > b):", !(a > b));
