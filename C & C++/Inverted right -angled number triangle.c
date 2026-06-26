59afb4d7c30b4970cda17aaa66683e25510e9bd7
SESHAGIRI VENKATA YASHWANTH <yashwanthhh34@gmail.com>
2026-06-26 20:51:31 +0530

    Add files via upload

---
+++ C & C++/Inverted right -angled number triangle.c
@@ -0,0 +1,15 @@
+//Inverted right-angled number triangle
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++)
+        printf("%d",j);
+        printf("\n");
+    }
+    return 0;
+}
+
